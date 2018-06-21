57e7a996be593938a818e4b3c2afd61a4ff0db5a
Yar <yarsher.dev@gmail.com>
2018-06-21 09:52:58 -0400

    Create ReminderTasks Class

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -11,17 +11,19 @@ public class ReminderTasks {
 public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
 
 // TODO (6) Create a public static void method called executeTask
-
-
+public static void executeTask(Context context, String action){
 // TODO (7) Add a Context called context and String parameter called action to the parameter list
-
 // TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+
+if (action == ACTION_INCREMENT_WATER_COUNT){
+    incrementWaterCount(context);
+}
+
+}
 
 // TODO (3) Create a private static void method called incrementWaterCount
     private static void incrementWaterCount(Context context){
 // TODO (4) Add a Context called context to the argument list
-
-
 // TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
         PreferenceUtilities.incrementWaterCount(context);
     }
