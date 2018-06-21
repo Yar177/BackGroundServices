74fbe702b2cdc94df1ac639fc14c3f803baaccea
Yar <yarsher.dev@gmail.com>
2018-06-21 14:26:36 -0400

    Finalizing Notification

--- app/src/main/java/com/example/android/background/MainActivity.java
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity implements
         startService(incrementWaterCountIntent);
     }
 
-    // TODO (15) Create a method called testNotification that triggers NotificationUtils' remindUserBecauseCharging
+    //  (15) Create a method called testNotification that triggers NotificationUtils' remindUserBecauseCharging
 
     @Override
     protected void onDestroy() {
