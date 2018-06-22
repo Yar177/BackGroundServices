15de875d0fe1c79c8a4c6b9e73c99929895076b7
Yar <yarsher.dev@gmail.com>
2018-06-22 11:41:14 -0400

    Update Min API to 21 Impliment Async tasj

--- app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
+++ app/src/main/java/com/example/android/background/sync/WaterReminderFirebaseJobService.java
@@ -1,9 +1,43 @@
 package com.example.android.background.sync;
 
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.os.AsyncTask;
+
 /**
  * Created by 10190270 on 6/22/2018.
  */
 
-public class WaterReminderFirebaseJobService {
+public class WaterReminderFirebaseJobService extends JobService {
 
+    private AsyncTask mBackgroundTask;
+
+
+    @Override
+    public boolean onStartJob(final JobParameters jobParameters) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Context context = WaterReminderFirebaseJobService.this;
+                ReminderTasks.executeTask(context, ReminderTasks.ACTION_CHARGING_REMINDER);
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(jobParameters, false);
+            }
+        };
+        mBackgroundTask.execute();
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+
+
+        return false;
+    }
 }
